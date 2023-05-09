729793f4117436ecaa9df022ccf3442e7b8cbad6
sooin <20200747@duksung.ac.kr>
2023-05-10 01:17:26 +0900

    nav&footer 완성

--- prensend/static/prensend/javascripts/navbar.js
+++ prensend/static/prensend/javascripts/navbar.js
@@ -4,10 +4,12 @@ const navbar = () => {
     if (num%2==0){
         document.getElementById("list").style.display = "none";
         document.getElementById("nav").style.display = "block";
+        document.getElementById("darkside").style.display = "block";
     }
     else {
         document.getElementById("list").style.display = "block";
         document.getElementById("nav").style.display = "none";
+        document.getElementById("darkside").style.display = "none";
     }
     num += 1;
 }
