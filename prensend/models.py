ec8e1dd1d57dd8fb99eb9ee8d75b401b0fe87d55
JiHyun Kim <kjh010524@gmail.com>
2023-05-13 23:00:45 +0900

    Item 모델 생성

--- prensend/models.py
+++ prensend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    brand = models.CharField(max_length=30)
+    price = models.CharField(max_length=30, blank=True)
+
+    def __str__(self):
+        return self.pk
+
+    def get_absolute_url(self):
+        return f'/prensend/{self.pk}/'
