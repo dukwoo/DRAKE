02a18b0668b6c3b439c79b4828d8e123d829a8ec
sooin <20200747@duksung.ac.kr>
2023-05-21 01:08:19 +0900

    mysql db 연동

--- DRAKE_project/settings.py
+++ DRAKE_project/settings.py
@@ -75,13 +75,25 @@ WSGI_APPLICATION = 'DRAKE_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
+# MySQL db 연동
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'frensend',
+        'USER': 'root',
+        'PASSWORD': 'dladyd34##A',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
---
+++ frensend.sql
@@ -0,0 +1,2 @@
+show databases;
+create database FRENSEND character set utf8mb4 collate utf8mb4_general_ci
