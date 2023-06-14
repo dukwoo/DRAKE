9709c28bcdefb611672439c849683bbf59ad4cc1
sooin <20200747@duksung.ac.kr>
2023-06-14 23:49:43 +0900

    db password modified

--- DRAKE_project/settings.py
+++ DRAKE_project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'frensend',
         'USER': 'root',
-        'PASSWORD': 'dladyd34##A',
+        'PASSWORD': 'Drake12!@',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
