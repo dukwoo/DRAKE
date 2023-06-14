33e8354ab7be2e7c3f61e45d8bbbb6ca4c62ed89
Yimyejin <loveim0112@naver.com>
2023-06-15 01:52:31 +0900

    로그인/로그아웃 기능 추가, 회원가입 기능 수정중

--- DRAKE_project/urls.py
+++ DRAKE_project/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('prensend.urls')),
     path('admin/', admin.site.urls),
+    path('common/', include('common.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
