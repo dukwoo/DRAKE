95df984712e0a546b5f6d7d3340569c092049711
JiHyun Kim <kjh010524@gmail.com>
2023-06-16 23:29:33 +0900

    상위 3개 추출 함수 추가

--- prensend/views.py
+++ prensend/views.py
@@ -28,6 +28,21 @@ def mypage(request):
         'prensend/mypage.html'
 )
 
+#head가 안되어서 상위 3개만 읽어오는 함수 만들어봄
+def get_top_n(list_a, num):
+    '''
+    list_a: [int, int, ...]
+    num: int, 추출하고 싶은 개수
+    '''
+    tmp = list_a.copy()
+    tmp.sort()
+
+    top_num = tmp[-num:]  # 뒤에서부터 추출
+    top_idx = [list_a.index(x) for x in top_num]
+
+    return top_idx
+
+
 #추천 알고리즘 (상위 3개만 가져오기, 상품명, 가격, 이미지, 링크 다 따로따로 리스트 만들어서 가져와야하나?)
 def get_filtered_items(age, gender, price):
     items = Item.objects.all()
@@ -54,7 +69,7 @@ def get_filtered_items(age, gender, price):
 
     if price == '1':
         result_list.append(items.filter(Q(category=s_age) & Q(price__lte = 10000)).order_by('rank'))
-        #카테고리가 20M 이어야하고 price가 10000이하인걸
+        #카테고리가 20M 이어야하고 price가 10000이하
 
     elif price == '12':
         result_list.append(items.filter(Q(category=s_age) & Q(price__gt = 10000) & Q(price__lt = 30000)).order_by('rank'))
@@ -70,8 +85,8 @@ def get_filtered_items(age, gender, price):
 
     # 현재 문제점 : 추천 알고리즘 완성, 이 부분은 교수님 피셜 문제없다고 확인받음
 
-    # 문제는 필터링된 데이터 리스트에서 상위 3개를 뽑는 것 (head쓰면 오류남)
-    # 앞선 상위 3개 뽑는 것이 되지 않아서(추정) 상위 3개 데이터를 recommend 페이지에서 보이게 하는 것이 안됨
+    # 필터링된 데이터 리스트에서 상위 3개를 뽑는 부분 head쓰면 오류나서 함수 추가해서 씀
+    # 문제는 상위 3개 데이터를 recommend 페이지에서 보이게 하는 것이 안됨
     # 앞서 quiz에서 선택한 데이터는 읽어오지만, 아이템을 읽어오지 못하는 것 같음
 
     # 모델에서 Char 형식으로 rank를 입력받아 order by를 rank로 하지 못하는 것이 의심됨 -> 그러나 Int 형식으로 모델 수정하면 오류남.
@@ -84,8 +99,10 @@ def quizinfo_index(request):
         gender = request.POST.get('search_mode_gender')
         price = request.POST.get('search_mode_price')
 
-        filtered_items = get_filtered_items(age, gender, price)
-        #3개만 출력하기 위해서는 여기에 head(3)을 해주어야하는데 하면 오류남ㅠㅠ
+        filtered_item = get_filtered_items(age, gender, price)
+        filtered_items = get_top_n(filtered_item, 3)
+
+        #3개만 출력하기 위해서는 여기에 head(3)을 해주어야하는데 하면 오류나서 함수 추가하여 사용하는 방향으로 바꿈
 
         context = {
             'filtered_items': filtered_items,
