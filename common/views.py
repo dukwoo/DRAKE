1429ccdaac1c6c7afc5ba8afa871ab9fca212cfb
Yimyejin <loveim0112@naver.com>
2023-06-17 18:10:10 +0900

    회원가입 기능 수정중

--- common/views.py
+++ common/views.py
@@ -1,26 +1,19 @@
-from django.contrib import auth
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from common.forms import UserForm
 from django.contrib.auth.forms import UserCreationForm
 
 
-# Create your views here.
 def signup(request):
-    """
-    회원가입
-    """
     if request.method == "POST":
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1'],
-                email=request.POST['email'],
-            )
-            auth.login(request, user)
-            return redirect('/')
-        return render(request, 'common/signup.html')
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)  # 사용자 인증
+            login(request, user)  # 로그인
+            return redirect('prensend')
     else:
-        form = UserCreationForm
-        return render(request, 'common/signup.html', {'form':form})
+        form = UserForm()
+    return render(request, 'common/signup.html', {'form': form})
