bbd998a840f2d0289ad1447252d718bfaeba994a
sooin <20200747@duksung.ac.kr>
2023-06-22 00:09:50 +0900

    views.py 수정

--- prensend/views.py
+++ prensend/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render, redirect
 from .models import Item
 from .models import Item_csv
 
+import pandas as pd
+import numpy as np
+import warnings
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 # Create your views here.
 def prensend(request):
     return render(
@@ -49,72 +55,79 @@ def get_top_n(list_a, num):
 
     return top_idx
 
+def find_sim_name(df, sorted_ind, product_name, top_n=3):
+        #인자로 입력된 products_df DataFrame에서 'name' 칼럼이 입력된 product_name 값인 DataFrame 추출
+        title_product = df[df['name'] == product_name]
 
-#추천 알고리즘 (상위 3개만 가져오기, 상품명, 가격, 이미지, 링크 다 따로따로 리스트 만들어서 가져와야하나?)
-def get_filtered_items(age, gender, price):
+        #product_name을 가진 DataFrame의 index 객체를 ndarray로 반환하고
+        #sorted_ind 인자로 입력된 name_sim_sorted_ind 객체에서 유사도 순으로 top_n개의 index 추출
+        title_index = title_product.index.values
+        print("title_index: ", title_index)
+        similar_indexes = sorted_ind[title_index, :(top_n)]
+
+        #추출된 top_n index 출력. top_n idnex는 2차원 데이터.
+        #DataFrame 에서 index로 사용하기 위해선 1차원 array로 변경
+        print(similar_indexes)
+        similar_indexes = similar_indexes.reshape(-1)
+
+        return df.iloc[similar_indexes]
+
+#추천 알고리즘
+def get_filtered_items(price):
+    #2
+    warnings.filterwarnings('ignore')
+
+    #products = pd.read_csv('./product.csv', engine = 'python', encoding='cp949') #'euc-kr'
     items = Item.objects.all()
-    result_list = []
-    result_list2 = []
+    products_df = items[['name', 'price', 'category']]
 
-    s_age = '20M'
-    if(age == '1020') & (gender == 'man'):
-        s_age = '20M'
-    elif (age == '1020') & (gender == 'woman'):
-        s_age = '20W'
-    elif (age == '30') & (gender == 'man'):
-        s_age = '30M'
-    elif(age == '30') & (gender == 'woman'):
-        s_age = '30W'
-    elif (age == '40') & (gender == 'man'):
-        s_age = '40M'
-    elif (age == '40') & (gender == 'woman'):
-        s_age = '40W'
-    elif (age == '50') & (gender == 'man'):
-        s_age = '50M'
-    elif (age == '50') & (gender == 'woman'):
-        s_age = '50W'
-    
+    #매트릭스의 형태를 상품수, 상품명
+    #CountVectorizer를 적용하기 위해 공백문자로 word 단위가 구분되는 문자열로 반환
+    #상품명을 공백으로 나눠서 각각의 단어의 개수를 추출.
+    #3
+    products_df['name_literal'] = products_df['name'].apply(lambda x:('').join(x))
+    count_vect = CountVectorizer(min_df=0, ngram_range=(1,2))
+    name_mat = count_vect.fit_transform(products_df['name_literal'])
+    print(name_mat.shape)
+
+    name_sim = cosine_similarity(name_mat, name_mat)
+    print(name_sim.shape)
+    print(name_sim[:1])
+
+    name_sim_sorted_ind = name_sim.argsort()[:, ::-1]
+    print(name_sim_sorted_ind[:1])
+
+    #유사한 상품들 가져옴
+    similar_products = find_sim_name(products_df, name_sim_sorted_ind, '[즉시배송]MAGNETA mini(마그네타 미니) - 반려동물용 항산화 영양제', 3)
+    print(similar_products[['name', 'price', 'category']])
+
+    # 유사도 측정 후 유사한 상품들만 가져와서 2차 필터링 진행 (평점과 가격)
+    result_list = []
+
     if price == '1':
-        result_list.append(items.filter(Q(category=s_age) & Q(price__lte = 10000)))
+        result_list.append(similar_products.filter(Q(price__lte = 10000)))
         #카테고리가 20M 이어야하고 price가 10000이하
 
     elif price == '12':
-        result_list.append(items.filter(Q(category=s_age) & Q(price__gt = 10000) & Q(price__lt = 30000)))
+        result_list.append(similar_products.filter(Q(price__gt = 10000) & Q(price__lt = 30000)))
 
     elif price == '34':
-        result_list.append(items.filter(Q(category=s_age) & Q(price__gte = 30000) & Q(price__lt = 50000)))
+        result_list.append(similar_products.filter(Q(price__gte = 30000) & Q(price__lt = 50000)))
 
     elif price == '5':
-        result_list.append(items.filter(Q(category=s_age) & Q(price__gte = 50000)))
+        result_list.append(similar_products.filter(Q(price__gte = 50000)))
 
-    for i in range(3):
-        result_list2.append(result_list[0][i])
-
+    print("result_list: ", result_list)
     
-    return result_list2
-    # 이 리스트에는 필터링된 아이템들이 모두 담겨있음, 3개 X
-
-    # 현재 문제점 : 추천 알고리즘 완성, 이 부분은 교수님 피셜 문제없다고 확인받음
-
-    # 필터링된 데이터 리스트에서 상위 3개를 뽑는 부분 head쓰면 오류나서 함수 추가해서 씀
-    # 문제는 상위 3개 데이터를 recommend 페이지에서 보이게 하는 것이 안됨
-    # 앞서 quiz에서 선택한 데이터는 읽어오지만, 아이템을 읽어오지 못하는 것 같음
-
-    # 모델에서 Char 형식으로 rank를 입력받아 order by를 rank로 하지 못하는 것이 의심됨 -> 그러나 Int 형식으로 모델 수정하면 오류남.
-    # 그래서 result_list의 내용을 확인해보고 싶은데 print는 작동안함
+    return result_list
 
 def quizinfo_index(request):
     if request.method == 'POST':
-        age = request.POST.get('search_mode_age')
         # age = request.POST['search_mode_age']
-        gender = request.POST.get('search_mode_gender')
         price = request.POST.get('search_mode_price')
 
-        filtered_items = get_filtered_items(age, gender, price)
-
+        filtered_items = get_filtered_items(price)
         #filtered_items = get_top_n(filtered_item, 3)
-
-        #3개만 출력하기 위해서는 여기에 head(3)을 해주어야하는데 하면 오류나서 함수 추가하여 사용하는 방향으로 바꿈
 
         context = {
             'filtered_items': filtered_items,
