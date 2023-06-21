6d8bddacf09b476c5773e41163d802a544a651ba
sooin <20200747@duksung.ac.kr>
2023-06-22 03:23:45 +0900

    2차원

--- prensend/views.py
+++ prensend/views.py
@@ -117,14 +117,15 @@ def get_filtered_items(price):
     elif price == '5':
         result_list.append(similar_products.query("price < 100000").values.tolist())
 
-    print(result_list[0][1])
-    result_list2.append(result_list[0][1])
-    print(result_list2)
+    #print(result_list[0][1])
+    #result_list2.append(result_list[0][1])
+    #print(result_list2)
 
     #필터링된 결과에서 최종적으로 별점순으로 정렬 후 추출.
     #.sort_values('rate', ascending=False) 값에 평점 높은 순으로 정렬 적용.
 
-    return result_list2
+    return result_list[0][1]
+    #return result_list2
 
 def quizinfo_index(request):
     if request.method == 'POST':
