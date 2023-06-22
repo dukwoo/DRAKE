83b5fdc26f0fa7fc8116f731d61f70305344eda4
Yimyejin <loveim0112@naver.com>
2023-06-23 01:29:10 +0900

    수정

--- prensend/views.py
+++ prensend/views.py
@@ -106,11 +106,15 @@ def get_filtered_items(titleArray1, titleArray2, titleArray3, price):
     print("name_sim_sorted_ind[:1] : ", name_sim_sorted_ind[:1], "\n")
 
     #유사한 상품들 가져옴
+    similar_products = find_sim_name(products_df, name_sim_sorted_ind, "탠디본사 소가죽 샌들, 9122039 2color 택1 (2.5cm)", 3)
+    print("similar_products1: ", similar_products[['rank', 'title', 'price', 'image', 'link', 'rate']], "\n")
+    
+    """
     rows = 9
     cols = 7
     similar_products = [[0] * cols] * rows
     
-
+    
     similar_products1 = find_sim_name(products_df, name_sim_sorted_ind, titleArray1, 3)
     print("similar_products1: ", similar_products1[['rank', 'title', 'price', 'image', 'link', 'rate']], "\n")
     
@@ -132,6 +136,7 @@ def get_filtered_items(titleArray1, titleArray2, titleArray3, price):
         
     for i in range(3):
         similar_products[i+6].append(similar_products3[i])
+    """
     
     result_list = []
 
