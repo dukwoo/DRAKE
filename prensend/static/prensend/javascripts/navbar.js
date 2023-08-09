6a9dae9902e68e89f3f7877ffd2e2edaf161698f
sooin <20200747@duksung.ac.kr>
2023-08-10 00:41:36 +0900

    navbar

--- prensend/static/prensend/javascripts/navbar.js
+++ prensend/static/prensend/javascripts/navbar.js
@@ -1,6 +1,19 @@
 var num = 0;
-const navbar = () => {
-    
+document.addEventListener("DOMContentLoaded", function(){
+    document
+        .querySelector("#list")
+        .addEventListener("click", function(e){
+            if (document.querySelector(".nav").classList.contains("on")){
+                //메뉴 slide
+                document.querySelector(".nav").classList.remove("on");
+            } else {
+                //메뉴 slidein
+                document.querySelector(".nav").classList.add("on");
+            }
+        });
+});
+
+/*const navbar = () => {
     if (num%2==0){
         document.getElementById("list").style.display = "none";
         document.getElementById("nav").style.display = "block";
@@ -12,4 +25,4 @@ const navbar = () => {
         document.getElementById("darkside").style.display = "none";
     }
     num += 1;
-}
+}*/
